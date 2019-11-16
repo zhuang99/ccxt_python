c8be367500ff629c0bfb529ed53c3df1d9073139
zhuang99 <zilie_huang@umail.ucsb.edu>
2019-11-16 01:49:25 -0800

    add ohlcy for bitmex

--- deribit/create_order.py
+++ deribit/create_order.py
@@ -10,10 +10,10 @@ deribit = ccxt.deribit({
 })
 
 
-# print(exchange.fetch_balance())
+
 symbol = 'BTC-PERPETUAL'
 type = 'market'
-side = 'sell'
+side = 'buy'
 amount = 100 / 10   #post的order size 为amount的10倍， 不知道为什么..
 price = None
 params = {
@@ -23,3 +23,5 @@ params = {
 order = deribit.create_order(symbol, type, side, amount, price, params)
 
 print(order)
+
+# print(deribit.fetch_my_trades(symbol))
